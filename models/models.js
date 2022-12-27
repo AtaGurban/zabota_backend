0bc3e8affc02344559e7f9355abdcbba0d5beae8
Ata Gurban <ata-g@mail.ru>
2022-12-27 05:15:37 +0500

    fd

--- models/models.js
+++ models/models.js
@@ -7,7 +7,7 @@ const User = sequelize.define("user", {
   firstName: { type: DataTypes.STRING },
   lastName: { type: DataTypes.STRING },
   // email: {type:DataTypes.STRING, unique: true},
-  phone: { type: DataTypes.STRING, unique: true },
+  phone: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING },
   deleted: { type: DataTypes.BOOLEAN, defaultValue: false },
   customerService: { type: DataTypes.BOOLEAN, defaultValue: false },
